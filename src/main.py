303c2368c9fc49cbee86f496f464402954d76ea5
ThomasVy <vythomas97@gmail.com>
2021-01-19 15:14:48 -0700

    add processRequest method to process the server requests

--- src/main.py
+++ src/main.py
@@ -1,19 +1,35 @@
 import socket
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+from typing import no_type_check
+HOST = '192.168.56.1'  # Standard loopback interface address (localhost)
+PORT = 55921        # Port to listen on (non-privileged ports are > 1023)
 
-def main():
+def main() -> None:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
+        s.connect((HOST, PORT))
+        keepListening = True
+        while keepListening:
+            data = s.recv(1024)
+            data = data.decode('utf-8')
+            keepListening = processRequest(data)
+        
+
+
+def processRequest(data: str) -> bool:
+    print("Received", data)
+    request = data.split('\n')
+    requestType = request[0]
+    if (requestType == "get team name"):
+        pass #TODO
+    elif (requestType == "get code"):
+        pass #TODO
+    elif (requestType == "receive peers"):
+        pass #TODO
+    elif (requestType == "get report"):
+        pass #TODO
+    elif (requestType == "close"):
+        return False
+    return True
+
 
 
 if __name__ == '__main__':
